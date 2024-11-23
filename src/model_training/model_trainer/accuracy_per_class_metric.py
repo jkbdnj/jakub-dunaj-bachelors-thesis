c438a93ccdc12823f4e5bdbed5589e1de21c7235
Jakub Dunaj <127201682+jkbdnj@users.noreply.github.com>
2024-11-23 01:39:22 +0100

    Extends the accuracy_per_class_metric.py module
    This commit extends the accuracy_per_class_metric.py module. The challenge is to come up with a way how to update the variables when the tensors are symbolic. This means that the values in the tensors cannot be accessed directly. The keras.ops package gives many operations to works with symbolic tensors.
    
    Implemented by: Jakub Dunaj
    Refs.: Issue #20

--- src/model_training/model_trainer/accuracy_per_class_metric.py
+++ src/model_training/model_trainer/accuracy_per_class_metric.py
@@ -1,34 +1,65 @@
 """Nothing now."""
 
+import numpy
+import tensorflow as tf
 from keras.metrics import Metric
+from keras.src import ops
 
 
+# https://www.tensorflow.org/api_docs/python/tf/keras/Metric#add_variable
+# https://www.tensorflow.org/api_docs/python/tf/Variable
+# https://github.com/keras-team/keras/blob/v3.3.3/keras/src/metrics/metric.py
+# https://github.com/keras-team/keras/blob/master/keras/src/metrics/reduction_metrics.py
 # https://www.tensorflow.org/api_docs/python/tf/keras/Variable
 # https://github.com/keras-team/keras/blob/v3.3.3/keras/src/initializers/__init__.py
 # https://github.com/keras-team/keras/blob/v3.3.3/keras/src/initializers/constant_initializers.py
+# https://www.tensorflow.org/guide/tensor_slicing (manipulation with tensors)
+# https://github.com/keras-team/keras/blob/master/keras/src/ops/numpy.py#L322
+# https://github.com/keras-team/keras/blob/v3.6.0/keras/src/ops/numpy.py#L841
+# https://www.tensorflow.org/api_docs/python/tf/keras/ops
+# https://www.tensorflow.org/api_docs/python/tf/is_symbolic_tensor
 class AccuracyPerClassMetric(Metric):
     """Nothing now.
 
     Attributes:
         _total_per_class (keras.src.backend.Variable): Variable representing an array holding the
-        total number of predictions per class. The
+        total number of predictions per class.
 
     """
 
     def __init__(self, class_count: int, name="accuracy_per_class_metric", dtype=None):
         """Nothing now."""
-        # dtype is float32 per default (result of the metric)
         super().__init__(name=name, dtype=dtype)
+        self._class_count = class_count
         self._total_per_class = self.add_variable(
-            shape=(class_count), initializer="zeros", dtype="int", name="total_per_class"
+            shape=(class_count,), initializer="zeros", dtype="int", name="total_per_class"
         )
 
         self._correct_per_class = self.add_variable(
-            shape=(class_count), initializer="zeros", dtype="int", name="correct_per_class"
+            shape=(class_count,), initializer="zeros", dtype="int", name="correct_per_class"
         )
 
-    def update_state():
-        """Nothing now."""
+    def update_state(self, y: tf.Tensor, y_pred: tf.Tensor, sample_weight=None):
+        """Nothing now.
 
-    def result() -> list[float]:
+        The symbolic tensors cannot be converted to a numpy array and the values cannot be accessed.
+        The operations have to be done with symbolic tensors rather than with numpy arrays.
+
+        Args:
+            y (tf.Tensor): This parameter is a symbolic rank 1 tensor of shape (batch_size,)
+            and contains the true labels of inputs.
+            y_pred (tf.Tensor): This parameter is a symbolic rank 2 tensor of shape (batch_size,
+            number_of_classes) and contains the predicted probabilities for each input for all
+            classes.
+            sample_weight: Nothing now.
+
+        """
+        # https://www.tensorflow.org/api_docs/python/tf/keras/ops/argmax
+        # the axis is index within the shape of the tensor
+        y_pred = ops.argmax(y_pred, axis=-1)
+        ops.equal(y, y_pred)
+
+    def result(self) -> list[float]:
         """Nothing now."""
+        # using map on those 2 arrays with the division operation
+        return numpy.zeros(38)
