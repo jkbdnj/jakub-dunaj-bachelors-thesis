47d6d8709392de0734158683cf957ce8463d1e30
Jakub Dunaj <127201682+jkbdnj@users.noreply.github.com>
2024-11-25 19:03:57 +0100

    Modify accuracy_per_class_metric.py module
    This commit modifies the accuracy_per_class_metric.py module.
    
    Implemented by: Jakub Dunaj

--- src/model_training/model_trainer/accuracy_per_class_metric.py
+++ src/model_training/model_trainer/accuracy_per_class_metric.py
@@ -1,7 +1,6 @@
 """Module providing custom accuracy per class metric."""
 
 import tensorflow as tf
-from keras.backend import epsilon
 from keras.metrics import Metric
 from keras.src import ops
 
@@ -51,28 +50,30 @@ class AccuracyPerClassMetric(Metric):
     def update_state(self, y: tf.Tensor, y_pred: tf.Tensor, sample_weight=None):
         """Function updating the _total_per_class and _correct_per_class instance attributes.
 
-        This method accumulates the statistics for the accuracy per class metric.The symbolic
-        tensors cannot be converted to a numpy array and the values cannot be accessed. The
-        operations have to be done with symbolic tensors rather than with numpy arrays. The
-        reset_state() method is implemented by the superclass Metric.
+        This method accumulates the statistics for the accuracy per class metric. The symbolic
+        tensors cannot be converted to a numpy array and the values cannot be accessed directly.
+        The operations have to be done with symbolic tensors. Converting the symbolic tensors into
+        numpy array does not work.
 
         Args:
             y (tf.Tensor): This parameter is a symbolic 1D (rank 1) tensor of shape (batch_size,)
             and contains the true labels of inputs.
             y_pred (tf.Tensor): This parameter is a symbolic 2D (rank 2) tensor of shape
-            (batch_size, number_of_classes) and contains the predicted probabilities for
-            each input for all classes.
-            sample_weight: Weights for batch elements. It is ignored.
+            (batch_size, class_count) and contains for each batch element predicted probabilities
+            across all classes in the dataset.
+            sample_weight: Weights for batch elements. Ignored in this case.
 
         """
-        # axis is index within the shape of a tensor (y_pred is a 2D tensor)
+        # returns the index of max value within each row of 2D tensor
+        # result is 1D tensor with labels with highest probabilities for each batch element
         label_predictions = ops.argmax(y_pred, axis=-1)
 
-        # returns 1D tensor containing correct predictions per batch element
+        # returns 1D tensor containing boolean values
+        # contains True if the prediction equals label at an index, otherwise False
         correct_predictions = ops.equal(y, label_predictions)
 
         for label in range(self._class_count):
-            # returns 1D tensor containing 1s at indices the label is at in y tensor
+            # returns 1D tensor containing True at indices the label is at in y tensor
             label_flags = ops.equal(y, ops.full(ops.shape(y), label))
 
             # returns 1D tensor containing the number of occurrences of label in batch
@@ -91,7 +92,7 @@ class AccuracyPerClassMetric(Metric):
             )
 
             # makes logical and between correct_predictions and label_flags
-            # returns tensor containing 1s where the prediction for label is correct
+            # returns tensor containing True at indices where the label is correctly predicted
             correct_label_flags = ops.logical_and(correct_predictions, label_flags)
 
             # returns 1D tensor containing the number of correct predictions of label in batch
@@ -110,5 +111,11 @@ class AccuracyPerClassMetric(Metric):
             )
 
     def result(self):
-        """Function computing current metric value."""
-        return ops.divide(self._correct_per_class, self._total_per_class + epsilon())
+        """Function computing current accuracy per class metric.
+
+        This function divides the _correct_per_class and _total_per_class tensors (variables) of
+        shape (class_count,) element-wise returning the accuracy for every class/label. The
+        ops.divide_no_nan() function does not return exception when dividing by 0.
+
+        """
+        return ops.divide_no_nan(self._correct_per_class, self._total_per_class)
